0d06f93d97da83b1f0dd0a35af048c530693302d
SrAranha <sraranhagameplays@gmail.com>
2020-08-07 23:54:17 -0300

    Added verification process

--- src/commands/Poll.js
+++ src/commands/Poll.js
@@ -1,25 +1,29 @@
-const { MessageReaction, MessageEmbed } = require("discord.js");
-
 module.exports = {
     name: 'poll',
     description: 'Starts an poll',
     execute(message, args) {
         message.delete();
-        const pollName = args.slice(0).join(' ');
-        const pollAuthor = message.author;
-        const pollEmbed = {
-            color: '#df8edd',
-            title: `${pollName}`,
-            author: {
-                name: `Criado por: ${pollAuthor.username}`,
-                icon_url: `${pollAuthor.avatarURL()}`,
-            },
-            timestamp: new Date(),
-        };
-        if (pollName == '') {
-            message.channel.send('Por favor, coloque o que está em votação após o comando!')
-        } else if (
-        message.channel.send({embed: pollEmbed}).then(message => message.react('✅').then(() => message.react('❌'))));
-        ;
+
+        if (message.author.roles.cache.some(role => role.name === 'Moderadores' || role.name === 'QueBom')) { //Check if the author has autorization
+            const pollName = args.slice(0).join(' ');
+            const pollAuthor = message.author;
+            const pollEmbed = {
+                color: '#df8edd',
+                title: `${pollName}`,
+                author: {
+                    name: `Criado por: ${pollAuthor.username}`,
+                    icon_url: `${pollAuthor.avatarURL()}`,
+                },
+                timestamp: new Date(),
+            };
+            if (pollName == '') {
+                message.channel.send('Por favor, coloque o que está em votação após o comando!')
+            }
+            else if (
+            message.channel.send({embed: pollEmbed}).then(message => message.react('✅').then(() => message.react('❌'))));
+        }
+        else {
+            message.channel.send(`${message.author}, você não tem permissão para usar esse comando!`)
+        }
     }
 }
