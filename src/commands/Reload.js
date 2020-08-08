9971247e01403bed21b81b73a2c3c40fd04091f1
SrAranha <sraranhagameplays@gmail.com>
2020-08-07 23:54:35 -0300

    Translated for PT-BR

--- src/commands/Reload.js
+++ src/commands/Reload.js
@@ -4,16 +4,16 @@ module.exports = {
 	args: true,
 	execute(message, args) {
 		//Checks if its me (Sr_Aranha) who passes the command
-		if (!message.author.id === '227887537102782464') return message.channel.send('You are not allowed to use this command');
+		if (!message.author.id === '227887537102782464') return message.channel.send(`${message.autho}, você não tem permissão para utilizar este comando`);
 
-		if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
+		if (!args.length) return message.channel.send(`Você não passou comando algum para recarregar, ${message.author}!`);
 		
 		const commandName = args[0].toLowerCase();
 		const command = message.client.commands.get(commandName)
 			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
 		if (!command) {
-			return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+			return message.channel.send(`Não tem comando com o nome \`${commandName}\`, ${message.author}!`);
 		}
 
 		delete require.cache[require.resolve(`./${command.name}.js`)];
@@ -21,10 +21,10 @@ module.exports = {
 		try {
 			const newCommand = require(`./${command.name}.js`);
 			message.client.commands.set(newCommand.name, newCommand);
-			message.channel.send(`Command \`${command.name}\` was reloaded!`);
+			message.channel.send(`O comando \`${command.name}\` foi recarregado!`);
 		} catch (error) {
 			console.log(error);
-			message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+			message.channel.send(`Teve um erro ao recarregar o comando \`${command.name}\`:\n\`${error.message}\``);
 		}
 	},
 };
