23bdace26d9268ddbf46834da73e3c823a16ffed
SrAranha <sraranhagameplays@gmail.com>
2020-08-11 13:11:08 -0300

    Added embed notification

--- src/commands/Punir.js
+++ src/commands/Punir.js
@@ -5,7 +5,17 @@ module.exports = {
         if (message.member.roles.cache.some(role => role.name === 'QueBom' || role.name === 'Moderadores')) { //Checks if user has permissions
             const member = message.mentions.members.first();
             const role = message.guild.roles.cache.find(role => role.name === 'Punidos');
-            member.roles.add(role);
+            const punishEmbed = {
+                color: '#865400',
+                title: `Membro punido: ${member.user.tag}`,
+                author: {
+                    name: `${message.author.tag}`,
+                    icon_url: `${message.author.avatarURL()}`,
+                },
+                timestamp: new Date(),
+            };
+            member.roles.add(role); 
+            message.channel.send({embed: punishEmbed});
         }
         else (message.channel.send(`${message.author}, você não pode usar este comando!`))
     }
