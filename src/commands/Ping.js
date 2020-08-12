4db84f98ebd798f0de14d2c56441ed028e710834
SrAranha <sraranhagameplays@gmail.com>
2020-08-12 18:39:46 -0300

    Added time in ms

--- src/commands/Ping.js
+++ src/commands/Ping.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(message) {
-		message.channel.send('Pong.');
+		const ms = new Date().getMilliseconds();
+		message.channel.send(`Pong! ${ms}ms (Ainda não é um comando confiável)`);
 	},
 };
