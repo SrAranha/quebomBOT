1975695e0772fb8f8a69f5dd523d2ac71a1f998d
SrAranha <sraranhagameplays@gmail.com>
2020-08-17 12:48:40 -0300

    Created

---
+++ src/commands/Delete.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'delete',
+    description: 'Deletes an amount of messages',
+    execute(message, args) {
+        if (message.member.roles.cache.some(role => role.name === 'QueBom' || role.name === 'Moderadores')) { //Checks if user has permissions
+            message.delete()
+            const messagesTarget = args.slice(0).join(' ');
+            message.channel.bulkDelete(messagesTarget);
+            message.channel.send(`Deleting ${messagesTarget} messages.`).then(msg => msg.delete({timeout: 3000}))
+            console.log('bomdia')
+        }
+        else (message.channel.send(`${message.author}, você não pode usar este comando!`))
+    }
+}
