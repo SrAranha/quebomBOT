17aebcd03c153054c0c4d519577851e09ec32ddd
SrAranha <sraranhagameplays@gmail.com>
2020-08-25 11:57:23 -0300

    Added multiple functionalities

--- src/commands/Mural.js
+++ src/commands/Mural.js
@@ -4,9 +4,36 @@ module.exports = {
     guildOnly: true,
     execute(message, args) {
         if (message.member.roles.cache.some(role => role.name === 'QueBom' || role.name === 'Moderadores')) { //Checks if user has permissions
-            message.delete() //deletes message calling the command
-            const messageId = args.slice(0, 1); //save the message id for react
-            message.channel.messages.fetch(`${messageId}`).then(msg => msg.react('✅')); //react the message with ✅
+            message.delete() //Deletes message calling the command
+            const muralArg = args.slice(0, 1); //Catch what to do
+            const channelMural = message.guild.channels.cache.find(channel => channel.id === '741305619297861643'); //ChannelMural
+                        
+            switch (muralArg[0].toLowerCase()) {
+                case 'abrir':
+                    message.channel.send('Mural está aberto!');
+                    channelMural.updateOverwrite(channelMural.guild.roles.everyone, { 
+                        'VIEW_CHANNEL': true,
+                        'SEND_MESSAGES': true,
+                        'ADD_REACTIONS': true,
+                        'READ_MESSAGE_HISTORY': true,
+                        'ATTACH_FILES':true,
+                        });
+                    break;
+                case 'fechar':
+                    message.channel.send('Mural está fechado!');
+                    channelMural.updateOverwrite(channelMural.guild.roles.everyone, { 
+                        'VIEW_CHANNEL': false,
+                        'SEND_MESSAGES': false,
+                        'ADD_REACTIONS': false,
+                        'READ_MESSAGE_HISTORY': false,
+                        'ATTACH_FILES':false,
+                        });
+                    break;
+                default: 
+                    message.channel.send(`${muralArg} (default)`);
+                    console.log(`${muralArg} (default)`);
+                    break;                               
+            }
         }
         else (message.channel.send(`${message.author}, você não tem permissão!`));
     }
