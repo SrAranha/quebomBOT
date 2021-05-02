0f605eb9396a0883f56f428d0bb5626b87794684
SrAranha <sraranhagameplays@gmail.com>
2021-05-01 21:10:25 -0300

    deleted test code

--- src/commands/Ping.js
+++ src/commands/Ping.js
@@ -1,9 +1,8 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
-	execute(message, client) {
+	execute(message) {
 		const ms = new Date().getMilliseconds();
 		message.channel.send(`Pong! ${ms}ms (quando tiver host certo vai tá bom)`);
-		message.channel.send(client.users.cache);
 	}
 }
