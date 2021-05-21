5befbbbdd00c937917b27808fdcc6590184f0acc
SrAranha <sraranhagameplays@gmail.com>
2021-05-21 01:02:26 -0300

    fixed it finally

--- src/commands/Canal.js
+++ src/commands/Canal.js
@@ -5,12 +5,10 @@ module.exports = {
     execute(message, args) {
         if (message.member.roles.cache.some(role => role.name === 'QueBom' || role.name === 'Moderadores')) { //Checks if user has permissions
             const toDo = args.slice(0);
-
-            if (toDo === String) {
                 switch (toDo[0].toLowerCase()) {
                     case 'privado': // Set channel private to public
                         message.delete();
-                        message.channel.send('Canal está sendo fechado!').then(msg => msg.delete({timeout: 3000}));
+                        message.reply('Este canal está sendo fechado!').then(msg => msg.delete({timeout: 3000}));
                         message.channel.updateOverwrite(message.channel.guild.roles.everyone, {
                             'VIEW_CHANNEL': false,
                             'SEND_MESSAGES': false,
@@ -20,7 +18,7 @@ module.exports = {
                         break;
                     case 'publico': // Set channel open to public
                         message.delete();
-                        message.channel.send('Canal está liberado ao público!').then(msg => msg.delete({timeout: 3000}));
+                        message.reply('Este canal está liberado ao público!').then(msg => msg.delete({timeout: 3000}));
                         message.channel.updateOverwrite(message.channel.guild.roles.everyone, { 
                             'VIEW_CHANNEL': true,
                             'SEND_MESSAGES': true,
@@ -30,22 +28,24 @@ module.exports = {
                         break;
                     case 'lento': // Set channel in slow mode
                         message.delete();
-                        if (isNaN(args[1])) return message.channel.send('Isso não é um número.').then(msg => msg.delete(    {timeout: 5000}));
-                        if (args[1] > 21600) return message.channel.send('Número inválido, deve ser menor que 21600.').then(msg     => msg.delete({timeout: 5000}));
-                        message.channel.send(`Canal está em modo lento de ${args[1]} segundos`).then(msg => msg.delete( {timeout: 3000}));
+                        if (isNaN(args[1])) return message.reply('Isso **não** é um número.').then(msg => msg.delete(    {timeout: 5000}));
+                        if (args[1] > 21600) return message.reply('Número inválido, deve ser menor que 21600.').then(msg     => msg.delete({timeout: 5000}));
+                        message.reply(`Canal está em modo lento de ${args[1]} segundos`).then(msg => msg.delete( {timeout: 3000}));
                         message.channel.setRateLimitPerUser(args[1]);
                         break;
                     case 'normal': // Set channel to normal speed
                         message.delete();
                         message.channel.setRateLimitPerUser(0);
-                        message.channel.send('O canal não está mais em modo lento').then(msg => msg.delete({timeout: 5000}));
+                        message.reply('O canal **não** está mais em modo lento').then(msg => msg.delete({timeout: 5000}));
                     case '+18': // Set channel to NSFW
                         message.delete();
                         message.channel.setNSFW(true);
+                        message.reply("O canal está em NSFW.").then(msg => msg.delete({timeout: 5000}));
                         break;
                     case '-18': // Set channel to SFW
                         message.delete();
                         message.channel.setNSFW(false);
+                        message.reply("O canal **não** está em NSFW.").then(msg => msg.delete({timeout: 5000}));
                         break;
                     default:
                         message.delete();
@@ -53,8 +53,6 @@ module.exports = {
                         break;
                 }
             }
-            else message.delete(); message.reply('comando está incompleto ou errado, as opções são `privado, publico, lento {segundos}, normal, +18, -18`.').then(msg => msg.delete({timeout: 5000}));
-        }
-        else (message.channel.send(`${message.author}, você não tem permissão!`));
+        else (message.reply(`${message.author}, você **não** tem permissão!`));
     }
 }
