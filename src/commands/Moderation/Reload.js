4470cccc7942d0d85010e7099837a4785905631d
SrAranha <sraranhagameplays@gmail.com>
2021-06-26 16:38:21 -0300

    Now working and and thx my dear friend

--- src/commands/Moderation/Reload.js
+++ src/commands/Moderation/Reload.js
@@ -1,61 +1,61 @@
 const fs = require("fs");
 
 module.exports = {
-  name: "reload",
-  aliases: ["restart", "rld"],
-  description: "Reloads a command",
-  args: true,
-  execute(message, args) {
-    const { AranhaBoladona_ID } = require("../../config.json");
-    //Checks if its me (Sr_Aranha) who passes the command
-    if (!message.author.id === AranhaBoladona_ID)
-        return message.reply("você não tem permissão para utilizar este comando");
+    name: "reload",
+    aliases: ["restart", "rld"],
+    description: "Reloads a command",
+    args: true,
+    execute(message, args) {
+        const { AranhaBoladona_ID } = require("../../config.json");
+        //Checks if its me (Sr_Aranha) who passes the command
+        if (!message.author.id === AranhaBoladona_ID)
+            return message.reply("você não tem permissão para utilizar este comando");
 
-    if (!args.length)
-        return message.reply("Você não passou comando algum para recarregar.");
+        if (!args.length)
+            return message.reply("Você não passou comando algum para recarregar.");
 
-    const commandName = args[0].toLowerCase();
-    const command =
-        message.client.commands.get(commandName) ||
-        message.client.commands.find(
-            (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
-        );
+        const commandName = args[0].toLowerCase();
+        const command =
+            message.client.commands.get(commandName) ||
+            message.client.commands.find(
+                (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+            );
 
-    if (!command) {
-        return message.reply(`Não tem comando com o nome \`${commandName}\`.`);
-    }
+        if (!command) {
+            return message.reply(`Não tem comando com o nome \`${commandName}\`.`);
+        }
 
-    const commandFolders = fs.readdirSync("./commands");
+        const commandFolders = fs.readdirSync("./commands");
 
-    let folderName;
+        let folderName;
 
-    commandFolders.map((folder) => {
-        fs.readdirSync(`${__dirname}/../${folder}/`).find((element) => {
-            if (element === `${command.name}.js`) {
-                delete require.cache[
-                require.resolve(`../${folder}/${command.name}.js`)
-                ];
+        commandFolders.map((folder) => {
+            fs.readdirSync(`${__dirname}/../${folder}/`).find((element) => {
+                if (element === `${command.name}.js`) {
+                    delete require.cache[
+                    require.resolve(`../${folder}/${command.name}.js`)
+                    ];
 
-            folderName = folder;
-            }
+                folderName = folder;
+                }
+            });
         });
-    });
 
-    try {
-        const newCommand = require(`../${folderName}/${command.name}.js`);
-        message.client.commands.set(newCommand.name, newCommand);
-        console.log("\x1b[35m%s\x1b[0m", `Command ${newCommand.name} was reloaded!`);
-    } catch (error) {
-        console.error(error);
-        message.channel.send(
-            `There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``
-        );
-    }
-    if (message.channel.type === 'dm') {
-        return;
-    }
-    else if (message.channel.type === 'text') {
-        message.delete();
-    }
-  },
+        try {
+            const newCommand = require(`../${folderName}/${command.name}.js`);
+            message.client.commands.set(newCommand.name, newCommand);
+            console.log("\x1b[35m%s\x1b[0m", `Command ${newCommand.name} was reloaded!`);
+        } catch (error) {
+            console.error(error);
+            message.channel.send(
+                `There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``
+            );
+        }
+        if (message.channel.type === 'dm') {
+            return;
+        }
+        else if (message.channel.type === 'text') {
+            message.delete();
+        }
+    },
 };
