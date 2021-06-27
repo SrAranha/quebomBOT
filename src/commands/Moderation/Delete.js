4705d676926b6ca2130ed01a081decc266eda6d9
SrAranha <sraranhagameplays@gmail.com>
2021-06-27 20:26:32 -0300

    updated, but theres more to add

--- src/commands/Moderation/Delete.js
+++ src/commands/Moderation/Delete.js
@@ -1,22 +1,24 @@
 module.exports = {
-    name: 'delete',
-    aliases: ['deletar', 'dlt'],
-    description: 'Deletes an amount of messages',
+    name: "delete",
+    aliases: ["deletar", "dlt"],
+    description: "Deletes an amount of messages",
     args: true,
     guildOnly: true,
     execute(message, args) {
-        //const roleMaxDel;
         if (message.member.hasPermission("ADMINISTRATOR") || message.member.hasPermission("MANAGE_MESSAGES")) { 
             const messagesTarget = parseInt(args.slice(0).join(' '));
+            if (!messagesTarget) {
+                message.reply("Você **não** proveu argumentos para o comando!");
+            }
             if (messagesTarget > 100) {
-                message.reply("O máximo de mensagens para serem deletadas é 100");
+                message.reply("O máximo de mensagens para serem deletadas é **100**");
             }
             else if (messagesTarget <= 100) { 
-                message.delete()
+                message.delete();
                 message.channel.bulkDelete(messagesTarget);
-                message.channel.send(`Deleting ${messagesTarget} messages.`).then(msg => msg.delete({timeout: 3000}));
+                message.channel.send(`Deletando ${messagesTarget} mensagens.`).then(msg => msg.delete({timeout: 3000}));
             }
         }   
-        else (message.reply('você **não** tem permissão para usar este comando!'));
+        else (message.reply("você **não** tem permissão para usar este comando!"));
     }
 }
