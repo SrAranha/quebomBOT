6d4d2e15f1c213194bd6c96fd9202e417b55ccb2
SrAranha <sraranhagameplays@gmail.com>
2021-09-08 22:06:54 -0300

    Improved currency search Added 'puppeteer' to auto search for currencies instead of me doing it.

--- src/commands/General/Currency.js
+++ src/commands/General/Currency.js
@@ -1,81 +1,67 @@
-const cInfo = require("./currencies.json");
+const puppeteer = require('puppeteer');
 module.exports = {
     name: "currency",
     aliases: ["cry"],
-    description: "Faz a conversão de algumas moedas para o Real.",
-    args: "{moeda} (quantidade)",
+    description: "Faz a conversão de algumas moedas para o Real. Favor seguir o padrão para que o comando funcione corretamente!",
+    args: "(quantidade) {moeda}",
     listArgs: ["MONEY", "COINS", "MOEDAS", "MOEDA"],
     execute(message, args, client) {
         //currencies infos
         const { queBomBOT_ID } = require("../../config.json");
         const ID = client.users.cache.get(queBomBOT_ID);
         
-        const currencies = cInfo.accepted;
+        const cry = args.slice(1).join(' ').toLowerCase();
+        const moneyAmount = parseInt(args.slice(0,1).join(' '));
         
-        const cry = args.slice(0,1).join(' ').toLowerCase();
-        const moneyAmount = parseInt(args.slice(1).join(' '));
+        async function searchWealth(a, b) {
+            if (!b) { b = 1 }
+            const browser = await puppeteer.launch({ headless: false});
+            const page = await browser.newPage();
+            const wealthURL = `https://www.google.com.br/search?q=${b}+${a}+para+real`;
+            await page.goto(wealthURL);
+            
+            const resultado = await page.evaluate(() => {
+                return document.querySelector('.a61j6.vk_gy.vk_sh.Hg3mWc').value;
+            });
 
-        if (currencies.includes(cry)) {
-            var result = moneyAmount * cInfo[cry].wealth;
-            var currencyColor = cInfo[cry].color;
-            var emojiFlag = cInfo[cry].flag;;
-        };
+            await browser.close();
 
-        if (!args[0]) { // Embed showing accepted currencies
-            var currencyEmbed = {
-                color: "#df8edd",
-                title: "Moedas suportadas pelo QueBomBOT.",
-                author: {
-                    name: `${ID.username}`,
-                    icon_url: `${ID.displayAvatarURL()}`,
-                },
-                fields: [
-                    {
-                        name: "Atualmente, temos estas moedas",
-                        value: `\`${currencies}\``,
+            if (!moneyAmount || moneyAmount < 2) { // Default embed to show base wealth compared to Real
+                var currencyEmbed = {
+                    color: '#df8edd',
+                    author: {
+                        name: `${ID.username}`,
+                        icon_url: `${ID.displayAvatarURL()}`,
                     },
-                ],
-                timestamp: new Date(),
+                    fields: [
+                        {
+                            name: `Um(a) ${cry.toUpperCase()} equivale à`,
+                            value: `${resultado} real(is) :flag_br:`,
+                        },
+                    ],
+                };
+                message.channel.send({embed: currencyEmbed});
             };
-            message.channel.send({embed: currencyEmbed});
-        };
-        
-        if (currencies.includes(cry) && !moneyAmount) { // Default embed to show base wealth compared to Real
-            var currencyEmbed = {
-                color: `${cInfo[cry].color}`,
-                author: {
-                    name: `${ID.username}`,
-                    icon_url: `${ID.displayAvatarURL()}`,
-                },
-                fields: [
-                    {
-                        name: `Um(a) ${cInfo[cry].name} ${cInfo[cry].flag} equivale à`,
-                        value: `${cInfo[cry].wealth} real(is) :flag_br:`,
+    
+            if (moneyAmount >= 2) { // Final embed showing the currency and its wealth
+    
+                var currencyEmbed = {
+                    color: '#df8edd',
+                    title: `Conversor de Moedas (${cry.toUpperCase()} -> BRL :flag_br:)`,
+                    author: {
+                        name: `${ID.username}`,
+                        icon_url: `${ID.displayAvatarURL()}`,
                     },
-                ],
-                footer: { text: `Última atualização em ${cInfo.last_updated}`}
+                    fields: [
+                        {
+                            name: `${cry.toUpperCase()} -> BRL :flag_br:`,
+                            value: `${moneyAmount} -> ${resultado}`,
+                        },
+                    ],
+                };
+                message.channel.send({embed: currencyEmbed});
             };
-            message.channel.send({embed: currencyEmbed});
-        };
-
-        if (currencies.includes(cry) && moneyAmount >= 2) { // Final embed showing the currency and its wealth
-
-            var currencyEmbed = {
-                color: `${currencyColor}`,
-                title: `Conversor de Moedas (${cry.toUpperCase()} ${emojiFlag}  -> BRL :flag_br:)`,
-                author: {
-                    name: `${ID.username}`,
-                    icon_url: `${ID.displayAvatarURL()}`,
-                },
-                fields: [
-                    {
-                        name: `${cry.toUpperCase()} ${emojiFlag} -> BRL :flag_br:`,
-                        value: `${moneyAmount} -> ${result}`,
-                    },
-                ],
-                footer: { text: `Última atualização em ${cInfo.last_updated}`}
-            };
-            message.channel.send({embed: currencyEmbed});
-        };
+        }
+        searchWealth(cry, moneyAmount);
     }
 }
