34091d7b2f45d20c8d9a93604ff36e53cd4fd1e7
SrAranha <sraranhagameplays@gmail.com>
2021-09-18 00:43:19 -0300

    Updated `qbCurrency`. Now it catches errors, and run independently of order

--- src/commands/General/Currency.js
+++ src/commands/General/Currency.js
@@ -8,64 +8,73 @@ module.exports = {
         //currencies infos
         const { queBomBOT_ID } = require("../../config.json");
         const ID = client.users.cache.get(queBomBOT_ID);
-        
-        const input = args.slice(0);        
-        const nbr = input.find(nbr => nbr >= 1);
-        const nbrIndex = input.indexOf(nbr)
-        input.splice(nbrIndex,1);
-        
+
+        var input = args;
+        var nbr = input.find(nbr => nbr >= 1);
+        var nbrIndex = input.indexOf(nbr);
+
+        if (nbrIndex+1 == input.length) {
+            var currency = input.splice(nbrIndex-1, 1);
+        }
+        else var currency = input.splice(nbrIndex+1);
+
         async function searchWealth(qnt, cry) {
             if (!qnt) { qnt = 1 }
             cry = cry.toString();
             var loading = "<a:qbloading:885334786254708796>";
-            message.channel.send(`${loading} Pegando as informações ${loading}`).then(msg => msg.delete({timeout: 2500}));
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            const wealthURL = `https://www.google.com.br/search?q=${qnt}+${cry}+para+real`;
+            message.channel.send(`${loading} Pegando as informações ${loading}`).then(msg => msg.delete({timeout: 2400}));
+            var browser = await puppeteer.launch();
+            var page = await browser.newPage();
+            var wealthURL = `https://www.google.com.br/search?q=${qnt}+${cry}+para+real`;
             await page.goto(wealthURL);
-            
-            const resultado = await page.evaluate(() => {
-                return document.querySelector('.a61j6.vk_gy.vk_sh.Hg3mWc').value;
-            });
 
-            await browser.close();
-
-            if (!nbr || nbr < 2) { // Default embed to show base wealth compared to Real
-                var currencyEmbed = {
-                    color: '#df8edd',
-                    author: {
-                        name: `${ID.username}`,
-                        icon_url: `${ID.displayAvatarURL()}`,
-                    },
-                    fields: [
-                        {
-                            name: `Um(a) ${cry.toUpperCase()} equivale à`,
-                            value: `${resultado} real(is) :flag_br:`,
-                        },
-                    ],
-                };
-                message.channel.send({embed: currencyEmbed});
-            };
+            try {
+                var resultado = await page.evaluate(() => {
+                    return document.querySelector('.a61j6.vk_gy.vk_sh.Hg3mWc').value;
+                });
     
-            if (nbr >= 2) { // Final embed showing the currency and its wealth
-    
-                var currencyEmbed = {
-                    color: '#df8edd',
-                    title: `Conversor de Moedas (${cry.toUpperCase()} -> BRL :flag_br:)`,
-                    author: {
-                        name: `${ID.username}`,
-                        icon_url: `${ID.displayAvatarURL()}`,
-                    },
-                    fields: [
-                        {
-                            name: `${cry.toUpperCase()} -> BRL :flag_br:`,
-                            value: `${nbr} -> ${resultado}`,
+                await browser.close();
+                
+                if (!nbr || nbr < 2) { // Default embed to show base wealth compared to Real
+                    var currencyEmbed = {
+                        color: '#df8edd',
+                        author: {
+                            name: `${ID.username}`,
+                            icon_url: `${ID.displayAvatarURL()}`,
                         },
-                    ],
+                        fields: [
+                            {
+                                name: `Um(a) ${cry.toUpperCase()} equivale à`,
+                                value: `${resultado} real(is) :flag_br:`,
+                            },
+                        ],
+                    };
+                    message.channel.send({embed: currencyEmbed});
                 };
-                message.channel.send({embed: currencyEmbed});
-            };
+                if (nbr >= 2) { // Final embed showing the currency and its wealth
+                    var currencyEmbed = {
+                        color: '#df8edd',
+                        title: `Conversor de Moedas (${cry.toUpperCase()} -> BRL :flag_br:)`,
+                        author: {
+                            name: `${ID.username}`,
+                            icon_url: `${ID.displayAvatarURL()}`,
+                        },
+                        fields: [
+                            {
+                                name: `${cry.toUpperCase()} -> BRL :flag_br:`,
+                                value: `${nbr} -> ${resultado}`,
+                            },
+                        ],
+                    };
+                    message.channel.send({embed: currencyEmbed});
+                }
+            } catch (error) {
+                message.reply("Desculpe, mas não foi possível pegar o resultado.");
+            }
         }
-        searchWealth(nbr, input);
+        if (!input) {
+            message.reply("Você não me deu o que fazer.");            
+        }
+        else { searchWealth(nbr, currency); };
     }
 }
