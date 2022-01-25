3842dcb7ffe17bafdadc02a1df36edc7025d0595
Danilo Aranha <sraranhagameplays@gmail.com>
2022-01-25 17:34:10 -0300

    Storing used commands. Now it store which command and the amount of times it has been used.

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,3 +1,5 @@
+const editJsonFile = require('edit-json-file');
+const { aranhaBoladona_ID } = require('../config.json');
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
@@ -6,6 +8,20 @@ module.exports = {
 		if (!command) return;
 		try {
 			command.execute(interaction);
+			console.log(`||Someone used ${command.data.name}||`);
+			if (interaction.user.id != aranhaBoladona_ID) {	// Storing used commands, except from me (Sr_Aranha)
+				let file = editJsonFile(`${__dirname}/commands.json`);
+				value = file.get(`timesUsed_${command.data.name}`);
+				if (value) {
+					value++;
+					file.set(`timesUsed_${command.data.name}`, value);
+				} else {
+					file.set(`timesUsed_${command.data.name}`, 1);
+				}
+				file.save();
+				file = editJsonFile(`${__dirname}/commands.json`, { autosave: true });
+			}
+
 		} catch (error) {
 			var red = '\x1b[31m%s\x1b[0m';
 			console.error(red, error);
