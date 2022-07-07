4664a892f88c846a950ece8d26ef7eda71752825
Danilo Aranha <sraranhagameplays@gmail.com>
2022-07-07 00:19:59 -0300

    Minor update to show when it started running.

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,7 +3,7 @@ module.exports = {
 	once: true,
 	execute(client) {
 		const green = '\x1b[32m%s\x1b[0m';
-		console.log(green,`${client.user.tag} is now online!`);
+		console.log(green,`${client.user.tag} is now online! ${new Date()}`);
         client.user.setActivity("created by Sr_Aranha", { type: "PLAYING"});
 	},
 };
